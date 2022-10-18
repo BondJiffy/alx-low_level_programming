25fbd24d516c63f2ec069e6891816e70702b588a
BondJiffy <isetesolomon@gmail.com>
2022-10-18 16:15:38 -0700

    My alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercae alphabets
+ */
+void print_alphabet(void)
+{
+	char aa;
+
+	for (aa = 'a'; aa <= 'z'; aa++)
+		_putchar(aa);
+	_putchar('\n');
+}
