5fa19d1fcfa7194a33851ef0d5772af356f0a796
BondJiffy <isetesolomon@gmail.com>
2022-10-19 16:18:17 -0700

    last digit commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * print_last_digit - prints last digit of a number
  * @x: the number whose last digit is to be printed
  * Return: last digit
  */
-int print_last_digit(int x);
+int print_last_digit(int x)
 {
 	int ld = x % 10;
 
@@ -13,3 +13,4 @@ int print_last_digit(int x);
 		ld *= -1;
 			_putchar(ld + '0');
 	return (0);
+}
