7f7fe032e71df1a47768fffba99f80f062b9e355
BondJiffy <isetesolomon@gmail.com>
2022-10-21 16:03:11 -0700

    My third commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+		_putchar(x * '0');
+
+	_putchar('\n');
+}
