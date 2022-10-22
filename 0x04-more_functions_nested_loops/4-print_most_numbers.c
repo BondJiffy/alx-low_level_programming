f5521f038f6753a3e4899565255aac1726557942
BondJiffy <isetesolomon@gmail.com>
2022-10-21 20:02:41 -0700

    My commit for task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print most numbers from 0 to 9, excluding 2 and 4
+ * print_most_numbers - print most numbers from 0 to 9
+ * excluding 2 and 4
  * Return: Always 0
  */
 
@@ -10,11 +11,11 @@ void print_most_numbers(void)
 	int m = 0;
 
 	while (m < 10)
+	{
 		if (m != 2 && m != 4)
-		{
 			_putchar(m + '0');
 
 			m++;
-		}
+	}
 	_putchar('\n');
 }
