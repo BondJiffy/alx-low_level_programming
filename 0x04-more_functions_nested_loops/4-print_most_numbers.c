a9e1930e554f65836ddec6056fede198382effe7
BondJiffy <isetesolomon@gmail.com>
2022-10-21 20:51:31 -0700

    my commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,17 +5,15 @@
  * without 2 and 4
  * Return: Always 0
  */
-
 void print_most_numbers(void)
 {
-	int m = 0;
+	int i = 0;
 
-	while (m < 10)
+	while (i < 10)
 	{
-		if (m != 2 && m != 4)
-			_putchar(m + '0');
-
-			m++;
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
