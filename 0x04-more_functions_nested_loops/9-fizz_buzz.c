b19007f047db41bced09cbe7370a4c8a853faabf
BondJiffy <isetesolomon@gmail.com>
2022-10-21 18:35:13 -0700

    fizzbuzz commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	printf("1");
+	for (x = 2; x <= 100; x++)
+	{
+		printf(" ");
+		if (x % 3 == 0)
+			printf("fizz");
+		if (x % 5 == 0)
+			printf("buzz");
+		if (x % 3 != 0 && x % 5 != 0)
+			printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
