20dc5c9d31704fc222d913e74c919b5c5ad4eb81
BondJiffy <isetesolomon@gmail.com>
2022-10-27 15:44:23 -0700

    string copy commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,16 +13,14 @@ char *_strcat(char *dest, char *src)
 	int b;
 
 	a = 0;
-
 	while (dest[a] != '\0')
 	{
 		a++;
 	}
 	b = 0;
-
 	while (src[b] != '\0')
 	{
-		dest[b] = src[b];
+		dest[a] = src[b];
 		a++;
 		b++;
 	}
