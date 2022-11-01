ffa76e596fa47e1ddb90f6addb660b45ffc8ed89
BondJiffy <113861754+BondJiffy@users.noreply.github.com>
2022-11-01 15:47:28 -0700

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,19 @@
-vgcduadhs
+#include "main.h"
+/**
+ * print_chessboard - print in the terminal the chessboard
+ * @a: The character to print
+ *
+ */
+void print_chessboard(char(*a)[8])
+{
+	int m, n;
+
+	for (m = 0; m < 8; m++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[m][n]);
+		}
+		_putchar('\n');
+	}
+}
