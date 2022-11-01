2494ee41f6e89e05daf0593024d285d8ab9e2934
BondJiffy <113861754+BondJiffy@users.noreply.github.com>
2022-11-01 15:34:42 -0700

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,24 @@
-vgcgucgkuaqcd
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: the memory area
+ * @c: constant byte
+ *
+ * Return: a pointer when the character is found
+ */
+char *_strchr(char *s, char c)
+{
+	int m;
+  
+  m= 0;
+
+	while (s[m] != '\0')
+	{
+		m++;
+		if (s[m] == c)
+		{
+			return (&s[m]);
+		}
+	}
+	return ('\0');
+}
