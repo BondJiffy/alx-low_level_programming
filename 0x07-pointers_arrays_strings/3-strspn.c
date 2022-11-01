af983f5733cf1540e400874226ed6ea2d9410c65
BondJiffy <113861754+BondJiffy@users.noreply.github.com>
2022-11-01 15:36:42 -0700

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,2 +1,33 @@
-vggdca
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer to a string
+ * @accept: pointer to a string
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+	int x = 0;
+	int v = 0;
 
+	while (s[x] != '\0')
+	{
+		while (accept[v] != '\0')
+		{
+			if (accept[v] == s[x])
+			{
+				count++;
+			}
+			v++;
+		}
+		v = 0;
+		x++;
+		if (s[x] == ' ')
+		{
+			break;
+		}
+	}
+	return (count);
+}
