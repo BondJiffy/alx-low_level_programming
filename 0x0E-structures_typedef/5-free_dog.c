2ab9696907c4e6c30e0f360d7a692525ef9e6954
BondJiffy <113861754+BondJiffy@users.noreply.github.com>
2022-11-14 14:38:09 -0800

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,16 @@
-#ifndef DOG_H
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Free dog
+ * @d: The dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
